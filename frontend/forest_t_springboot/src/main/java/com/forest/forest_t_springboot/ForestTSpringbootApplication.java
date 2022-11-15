1cfb09510740f95fadfe12ee136eb8020cff9929
henamdu <duweihengmr@163.com>
2022-11-15 14:52:05 +0800

    feat: first commit

---
+++ frontend/forest_t_springboot/src/main/java/com/forest/forest_t_springboot/ForestTSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.forest.forest_t_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestTSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForestTSpringbootApplication.class, args);
+    }
+
+}
---
+++ frontend/forest_t_springboot/src/test/java/com/forest/forest_t_springboot/ForestTSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forest.forest_t_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestTSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
