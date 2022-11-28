0e650472875e5219dab9906bd87d8b327d875097
henamdu <duweihengmr@163.com>
2022-11-28 20:36:29 +0800

    feat: add mybatis-plus config

---
+++ backend/forest_t_springboot/src/main/java/com/forest/springboot/config/MybatisPlusConfig.java
@@ -0,0 +1,27 @@
+package com.forest.springboot.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import org.apache.ibatis.reflection.MetaObject;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author: dwh
+ **/
+@Configuration
+@MapperScan("com.tfiip.aop.mapper")
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
+        return paginationInterceptor;
+    }
+
+}
