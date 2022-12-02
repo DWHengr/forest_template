a92c38fc8066f62c1cbbc158df74f037da417644
henamdu <duweihengmr@163.com>
2022-12-02 20:30:07 +0800

    feat: add components

---
+++ frontend/forest_t_vue3/src/components/ALogo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="logo">
+    <img src="@/assets/logo.svg" alt="" :class="{ 'mr-2': !collapsed }" />
+    <div v-show="!collapsed" class="logo-title">Forest</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ALogo',
+    props: {
+      collapsed: {
+        type: Boolean,
+        default: true,
+      },
+    },
+  };
+</script>
+
+<style lang="less" scoped>
+  .logo {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 64px;
+    line-height: 64px;
+    overflow: hidden;
+    white-space: nowrap;
+
+    img {
+      width: auto;
+      height: 32px;
+    }
+
+    .logo-title {
+      margin-bottom: 0;
+    }
+  }
+</style>
